322a157554ce6b54862c1818ca308c1cac99acce
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-08-25 11:48:51 +0530

    Update sql8.sql

--- sql8.sql
+++ sql8.sql
@@ -114,3 +114,107 @@ call addemp("Suraj",45000);
 
 select * from employees;
 
+delimiter //
+  create procedure deductsalary(in eid int, in deduct_salary int)
+  begin
+    declare current_salary int;
+    start transaction;
+    select salary into current_salary from teacher where tid = eid;
+    
+    if current_salary - deduct_salary < 0 then
+      rollback;
+    else
+      update teacher 
+      set salary = salary - deduct_salary
+      where tid = eid;
+      commit;
+    end if;
+  end //
+delimiter ;
+
+call deductsalary(3,1200);
+
+delimiter //
+	create procedure insertAndSavepoint()
+    begin
+		start transaction;
+        insert into teacher(tname, salary) values ("Sujal", 64000, 'M');
+        savepoint sp1;
+        insert into teacher(tname, salary) values ("Vijaya", 82000, 'F');
+        savepoint sp2;
+        rollback to sp1;
+        commit;
+	end //
+delimiter ;
+call insertAndSavepoint();
+
+delimiter //
+	create procedure safe_insert(in teach_name varchar(50), in sal int)
+    begin
+		declare exit handler for sqlexception
+        begin
+			rollback; -- will execute when query fails
+		end;
+		start transaction;
+        insert into teacher (tname, salary) values (teach_name, sal);
+        update teacher set salary = "ABC" where id = 1;
+        commit;
+	end //
+delimiter ;
+
+call safe_insert("Govinda", 72000);
+
+-- Handle DividebyZero exception
+
+delimiter //
+  create procedure safe_division(in a int, in b int,out result decimal(10,2))
+  begin
+     declare exit handler for sqlexception
+     begin
+      set result = null; -- set safe value
+     end;
+     
+     set result = a / b;
+  end //
+delimiter ;
+
+set @x = 0;
+call safe_division(1,0,@x);
+select @x as result;
+
+-- Handle DividebyZero exception
+
+delimiter //
+  create procedure safe_division1(in a int, in b int,out result decimal(10,2))
+  begin
+     declare exit handler for 1365
+     begin
+      set result = null; -- set safe value
+     end;
+     
+     set result = a / b;
+  end //
+delimiter ;
+
+set @x = 0;
+call safe_division1(1,0,@x);
+select @x as result;
+
+-- Handle DividebyZero exception
+
+delimiter //
+  create procedure get_studentname(in id int,out name varchar(50))
+  begin
+     declare continue handler for not found
+     begin
+      set name = "no record found"; -- set safe value
+     end;
+     
+     select sname into name from students where sid = id;
+  end //
+delimiter ;
+
+set @x = 0;
+call get_studentname(1,@x);
+select @x as student_name;
+
